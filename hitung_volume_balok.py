bb5f493583e0e376b41f9ca2e0380a66576ef4ec
aguswedi <143860445+aguswedi@users.noreply.github.com>
2023-09-06 20:30:58 +0700

    Add files via upload

---
+++ hitung_volume_balok.py
@@ -0,0 +1,11 @@
+#program menghitung volume balok
+
+print("program menghitung volume balok :")
+
+p = int(input("masukkan pajang balok :"))
+l = int(input("masukkan lebar balok :"))
+t = int(input("masukkan tinggi balok :"))
+
+v = p * l * t 
+print("rumus menghitung volume balok V = p x l x t")
+print("hasil dari menghitung volume balok :",v)
---
+++ hitung_volume_tabung.py
@@ -0,0 +1,11 @@
+# program menghitung volume tabung
+
+print("program menghitung volume tabung")
+
+pi = 22/7
+r = int(input("masukkan jari jari menghitung:"))
+t = int(input("masukkan tinggi tabung:"))
+
+v = pi * r** 2 * t
+print("rumus menghitung volume tabung V = pi x r**2 x t")
+print("hitung hasil volume tabung :", v)
